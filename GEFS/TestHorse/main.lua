d2c8d694a86591ab7e7c815c0d5142492384a4d1
Emily <ecmoeller@wisc.edu>
2019-12-09 02:15:44 -0600

    Added trees, trying to fix camera movement

--- GEFS/TestHorse/main.lua
+++ GEFS/TestHorse/main.lua
@@ -4,7 +4,7 @@ print("Animated Hourse with Several States (Walk, Dash, Jump, Fall)")
 -- frame and used to set the camera pose.
 --Here we set the intial camera pose:
 CameraPosX = -4.0; CameraPosY = 2; CameraPosZ = -1.8
-CameraDirX = 1.0; CameraDirY = -0.2; CameraDirZ = 0.4
+CameraDirX = 1.0; CameraDirY = -0.0; CameraDirZ = -0.0
 CameraUpX = 0.0; CameraUpY = 1.0; CameraUpZ = 0.0
 
 theta = 0; radius = 4 --Helper variabsle for camera control
@@ -31,6 +31,7 @@ end
 --Special function that is called every frame
 --The variable dt containts how much times has pased since it was last called
 function frameUpdate(dt)
+  frameDt = dt
   animate(dt)
 end
 
@@ -38,19 +39,48 @@ end
 --keys containts information about which keys are currently .
 function keyHandler(keys)
 
-  --Move camera radius and theta based on up/down/left/right keys
-  if keys.right then
-    theta = theta + .03
-  end
+  -- --Move camera radius and theta based on up/down/left/right keys
+  -- if keys.right then
+  --   theta = theta + 1*frameDt
+  -- end
+  -- if keys.left then
+  --   theta = theta - 1*frameDt
+  -- end
+  -- if keys.up then
+  --   CameraPosX = CameraPosX + CameraDirX*frameDt
+  --   CameraPosZ = CameraPosZ + CameraDirZ*frameDt
+  --   print("CameraPosX "..CameraPosX)
+  --   print("CameraPosZ "..CameraPosZ)
+  -- end
+  -- if keys.down then
+  --   CameraPosX = CameraPosX - CameraDirX*frameDt
+  --   CameraPosZ = CameraPosZ - CameraDirZ*frameDt
+  --   print("CameraPosX "..CameraPosX)
+  --   print("CameraPosZ "..CameraPosZ)
+  -- end
+
+  -- rotate camera to the left
   if keys.left then
-    theta = theta - .03
+    theta = theta + 1*frameDt
   end
+  -- rotate camera to the right
+  if keys.right then
+    theta = theta - 1*frameDt
+  end
+  -- move camera forward
   if keys.up then
-    radius = radius - .05
+    CameraPosX = CameraPosX + CameraDirX*frameDt
+    CameraPosZ = CameraPosZ + CameraDirZ*frameDt
   end
+  -- move camera backward
   if keys.down then
-    radius = radius + .05
+    CameraPosX = CameraPosX - CameraDirX*frameDt
+    CameraPosZ = CameraPosZ - CameraDirZ*frameDt
   end
+
+  -- apply theta changes
+  CameraDirX = math.cos(theta)
+  CameraDirZ = -math.sin(theta)
 
   --When tab is pressed hide the current animation and unhide the next one
   if keys.tab and not tabDownBefore then
@@ -71,13 +101,19 @@ end
 floor = addModel("Floor",0,.95,0)
 setModelMaterial(floor,"Flooring")
 
+--Try new floor
+--id = addModel("FloorPart",0,0,0)
+--placeModel(id,0,-.02,0)
+--scaleModel(id,10,1,10)
+--setModelMaterial(id,"Clay")
+
 --Load various animated models of horse actions
 i = 1 --Lau is typically 1-indexed
 horse = {}
 speed = {}
-horse[i] = addModel("Llama-Jump",0,1,0); speed[i] = 1.1; i = i+1
+horse[i] = addModel("Llama-Jump",0,1,0); speed[i] = 0.3; i = i+1
 horse[i] = addModel("Horse-Dash",0,1,0); speed[i] = 3.0; i = i+1
-horse[i] = addModel("Horse-Jump",0,1,0); speed[i] = 1.2; i = i+1 --No one velocity is right!
+horse[i] = addModel("Snake-Attack",0,1,0); speed[i] = 1.2; i = i+1 --No one velocity is right!
 horse[i] = addModel("Horse-Fall",0,1,0); speed[i] = 0.0; i = i+1
 
 --Only draw one of the actions
@@ -85,5 +121,15 @@ curAction = 1
 for i = 1,#horse do
   if curAction ~= i then
     hideModel(horse[i])
+  end
+end
+
+-- forest
+trees = {}
+idx = 1
+for k = -10, 10 do
+  for l = -10, 10 do
+    trees[idx] = addModel("Tree", k, 1, l)
+    idx = idx + 1
   end
 end
